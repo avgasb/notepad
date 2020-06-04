51b70602945ed8d295e1838fa491fd128bd600b8
avg_asb <avg_asb@mail.ru>
2020-06-05 01:01:56 +0300

    post class + ancestors

---
+++ link.rb
@@ -0,0 +1,15 @@
+class Link < Post
+  def initialize
+    super # дернули эти методы из родительского класса
+    @url = ''
+  end
+
+  def read_from_console
+
+  end
+
+  def to_string
+    
+  end
+
+end
---
+++ memo.rb
@@ -0,0 +1,14 @@
+class Memo < Post
+  # def initialize ЭТО НЕ НАД, Т.К. НИЧЕГО НОВОГО НЕ ОБЪЯВЛЯЕТСЯ, А ВСЕ И ТАК ПОДТЯНЕТСЯ ИЗ РОДИТЕЛЬСКОГО КЛАССА
+  #   super
+  # end
+
+  def read_from_console
+
+  end
+
+  def to_string
+
+  end
+
+end
